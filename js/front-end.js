c804af6a00389aad3251fd7b78fd8399f4936d60
Yogibuff <yogibuff@icloud.com>
2015-12-17 17:06:37 -0800

    created front-end javascript file and added a simple animate for the user input on page load

--- js/front-end.js
+++ js/front-end.js
@@ -2,3 +2,5 @@
 
 // initially hides the search bar, will animate into place from left to center.
 $('#searchField').hide().show('slow');
+
+//Create a map object and specify the DOM element for display
