197b360ec0c10080492897f344a8dfb25b1e8796
Yogibuff <yogibuff@icloud.com>
2015-12-17 17:10:51 -0800

    issue#6: require the Express module, routes for directory and server root, set up a local port to static host images and listen for requests.

---
+++ js/api.js
@@ -0,0 +1,62 @@
+// express app
+var express = require( 'express' ),
+  app = express(),
+  request = require( 'request' ),
+  router = express.Router();
+
+// body-parser middlewear
+var bodyParser = require( 'body-parser' ),
+  parseText = bodyParser.text();
+
+// Add a route for POST, with status codes (including success 200 code) logged to the console
+router.post( '/', parseText, function( req, res ) {
+  //"http://maps.googleapis.com/maps/api/js?key=YOUR_KEY"
+  var baseUrl = 'http://maps.googleapis.com/maps/api/js?';
+  var apiKey = 'AIzaSyCbgb6h0fxgC-2HjRmh6YlLnLiCC0edouw';
+  var additionalParameters = '&signed_in=true&libraries=places&callback=initMap';
+  /* additonal Parameters are If they're signed in with their Google account, their saved places, 
+  home and work locations, and more are built right into the map they see. This also means that 
+  interactions with the map, such as starring a location, are saved for easy viewing in Google Maps 
+  for desktop or mobile, and any other Google Map that the user visits on the web. */
+
+  var queryURL = baseUrl + /* req.body + */ 'key=' + apiKey + additionalParameters;
+
+  request( queryURL, function( error, response, body ) {
+    // if no error and status code 200, log success and send the recieved .json file to body-parser for stringify'd text response
+    if ( !error && response.statusCode == 200 ) {
+      console.log( 'Success. Status code: ' + response.statusCode );
+      res.send( body );
+    }
+    else {
+      console.log(error);
+      console.log( 'Status code: ' + response.statusCode );
+    }
+  });
+});
+
+module.exports = router;
+
+
+//////////////  Promises Synchronous order of operations  //////////////
+
+/* Node implementation of Promises - coding pattern to enforce proper Synchronous pattern of passing data, 
+so that user input location can be converted to longitude/latitude and THEN this value can be plugged 
+into the 'center' property of the google Map initializing function */
+
+// add this initMap() function into the DOM only AFTER the center property is set!
+
+  // var map;
+  // function initMap() {
+  //   var x =  /* latitude.State  */ ;
+  //   var y =  /* longitude.State */ ;
+  //   map = new google.maps.Map(document.getElementById('map'), {
+  //     center: {lat: x, lng: y},
+  //     zoom: 8
+  //   });
+  // }
+
+// FIRST Promise takes user input and matches it to a latitude / longitude value pair, 
+//    Alternate: having a list of preset states (UT, CO, CA, VT, MT, NM) with stored lat/long data that gets plugged in to init map function 
+// THEN pass this lat/long data into the function initMap() 'center' property
+// THEN request Google Maps API and post to the DOM
+// THEN request SnowCountry.com Snow Reports (left side of their website: #ski areas open, #areas with new snow, Biggest Snowfalls...) 
