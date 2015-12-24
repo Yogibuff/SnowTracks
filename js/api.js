1a08cfd6759604bee4af258cd88820185a8540b6
Yogibuff <yogibuff@icloud.com>
2015-12-24 04:39:59 -0800

    #9 Modify SnoCountry API URL, remove unneccessary code.

--- js/api.js
+++ js/api.js
@@ -1,34 +1,39 @@
 // express app
-var express = require( 'express' ),
-  app = express(),
-  request = require( 'request' ),
-  router = express.Router();
+var express = require('express'),
+    app = express(),
+    request = require('request'),
+    router = express.Router();
 
 // body-parser middlewear, to be used with SnowCountry API JSON returned data
-var bodyParser = require( 'body-parser' ),
-  parseText = bodyParser.text();
+var bodyParser = require('body-parser'),
+    parseText = bodyParser.text();
 
 
-/* 
-router.post( '/', function( req, res ) {
-//SnoCountry API reqest URL format (goes on the end) http://feeds.snocountry.net/conditions.php?apiKey=SnowCountry.example&states=ca 
 
-var SnoCountryApi = URL: http://feeds.snocountry.net/conditions.php?apiKey=SnoCountry.example
-request( SnoCountryApi, function( error, response, body ) {
+router.post( '/', function(req, res) {
+  // SnoCountry API reqest URL format (modifiers attached on the end, ex: '&states=ca')
+  // Add '&output=json' for Json formatted response
+  var SnoCountryQuery = "http:feeds.snocountry.net/conditions.php?apiKey=SnoCountry.example&states=ca&resortType=alpine&output=json";
+  request( SnoCountryQuery, function(error, response, body) {
     // if no error and status code 200, log success and send the recieved .json file to body-parser for stringify'd text response
     if ( !error && response.statusCode == 200 ) {
-      console.log( 'Success. Status code: ' + response.statusCode );
-      res.send( body );
+      console.log('Success. Status code: ' + response.statusCode);
+      res.send( parseText(body) );
     }
     else {
       console.log(error);
-      console.log( 'Status code: ' + response.statusCode );
+      console.log('Status code: ' + response.statusCode);
     }
   });
 });
 
 module.exports = router;
-*/
+
+
+// Syntax for snow conditions query for a specific resort:
+// URL: http://feeds.snocountry.net/conditions.php?...{Request Parameters}...
+// Or by State, using getResortList (returns html default, or add &output=JSON:
+// URL: http://feeds.snocountry.net/getResortList.php?...{Request Parameters}...
 
 
 //////////////  Promises Synchronous order of operations  //////////////
