f8c7748ae585682810dd284551c177a3ccea5eb5
Yogibuff <yogibuff@icloud.com>
2015-12-24 04:59:51 -0800

    #16 Routers setup. Root directory route set, Static Routes for Images, Javascript, and CSS. SnowCountry is setup but commented out, since it causes an error until api.js is correctly making requests from the API.

--- js/app.js
+++ js/app.js
@@ -3,34 +3,59 @@
 
 // require the express module, set to variable called app
 var express = require('express'),
-  app = express(),
-  path = require('path');
-var port = 3000;
+    app = express(),
+    path = require('path'),
+    port = 3000;
 
 // set a route for the root of the server
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/../index.html'));
-  console.log(req.path + " was requested.");
-  console.log(req.body);
 });
 
 // other routes
-app.get('/package.json', function(req, res) {
-  res.sendFile(path.join(__dirname + '/../package.json'));
-  console.log(req.path + " was requested.");
+app.get('/index.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/../index.html'));
 });
 
 app.get('/about-us.html', function(req, res) {
-res.sendFile(path.join(__dirname + '/../about-us.html'));
+  res.sendFile(path.join(__dirname + '/../about-us.html'));
 });
 
-//images
+app.get('/package.json', function(req, res) {
+  res.sendFile(path.join(__dirname + '/../package.json'));
+  // console.log(req.path + " was requested.");
+});
+
+// didn't work to fix the error
+// app.get('/node_modules', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/../node_modules'));
+//   // console.log(req.path + " was requested.");
+// });
+// // static server to serve node_module files
+// app.use('/node_modules', express.static('node_modules'));
+
+// CSS
+app.use('/css', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../css/default.css'));
+  next();
+});
+// static server to serve CSS files
+app.use('/css', express.static('css'));
+
+// images
 app.use('/images', function(req, res, next) {
-  console.log('Image' + ' ' + req.url + ' ' + 'was requested');
   next();
 });
 // static server to serve image files
 app.use('/images', express.static('images'));
+
+// Javascript
+app.use('/js', function(req, res, next) {
+  next();
+});
+// static server to serve js files
+app.use('/js', express.static('js'));
+
 
 // requires the api.js file for later implementation of snoCountry API
 // var api = require('/api.js');
@@ -39,5 +64,5 @@ app.use('/images', express.static('images'));
  //////////////////////////////////////////////////////////
 // hosted locally & listening for requests on port 3000 //
 app.listen( port, function(){
-	console.log("The front-end server is running on port " + port + " " + "Go to localhost:" + port + " to view this app!");
+  console.log("The front-end server is running on port " + port + " " + "Go to localhost:" + port + " to view this app!");
 } );
