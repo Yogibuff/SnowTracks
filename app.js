8a49747ef6c60686e13e30a5e8736a0da4d078b1
Yogibuff <yogibuff@icloud.com>
2016-01-03 17:45:11 -0800

    Fix #16 Set a static router to retrieve HTML, CSS, JS, and Image files on port 3000. Removed Path module, no longer needed.

--- app.js
+++ app.js
@@ -1,56 +1,22 @@
 /* NOTE: Does NOT work when placed in the js folder, even when adding /../ before routes! */
 
-// require the express module, set to variable called app
+// require the express module and set host port
 var express = require('express'),
     app = express(),
-    path = require('path'),
     port = 3000;
 
-// set a route for the root of the server
-app.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-  next();
-});
+// set a static router to retrieve HTML, CSS, JS, Images
+app.use(express.static(__dirname));
 
-// other routes
-app.get('/index.html', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-  next();
-});
-
-app.get('/about-us.html', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/about-us.html'));
-  next();
-});
-
-app.get('/package.json', function(req, res) {
-  res.sendFile(path.join(__dirname + '/package.json'));
-});
-
-// CSS
-app.use('/css', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/css/default.css'));
-  next();
-});
-// static server to serve CSS files
-app.use('/css', express.static('css'));
-// Javascript
-app.use('/js', function(req, res, next) {
-  // console.log(req.path + " was requested.");
-  next();
-});
-// static server to serve js files
-app.use('/js', express.static('js'));
-
-// static server to serve image files
-app.use('/images', express.static('images'));
-
-// requires the api.js file for later implementation of snoCountry API
+// require api.js for implementation of SnoCountry API's weather queries
 // var api = require('/api.js');
 // app.use('/api', api);
 
- //////////////////////////////////////////////////////////
-// hosted locally & listening for requests on port 3000 //
+
+  ////////////////////////////////////////////////////////
+ // Hosted locally & listens for requests on port 3000 //
+////////////////////////////////////////////////////////
+
 app.listen( port, function() {
   console.log("Front-end server is running." + " " + "Go to localhost:" + port + " to view SnowTracks!");
 } );
