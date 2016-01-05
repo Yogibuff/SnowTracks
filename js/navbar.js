a4d4aa490be7e5b8a0d2f35a88f5fdf77f82a3fb
Yogibuff <yogibuff@icloud.com>
2016-01-05 14:36:56 -0800

    #3 Remove unused code, and clean spacing.

--- js/navbar.js
+++ js/navbar.js
@@ -15,30 +15,12 @@ $(document.body).ready(function () {
 
 /* Sponsors */
 
-// add sponsor logos to .container #sponsors
-// function insertSponsors() {
-//   var sponsorImages = document.getElementById("sponsors").children;
-  
-//   // if sponsor image exists, remove it on click instead
-//   if (sponsorImages.length > 1) {
-//     sponsorImages.nodeValue = "";
-//   }
-//   else {
-//     sponsorImages[0].nodeValue = " <img src='images/snowbird-logo.png'> ";
-//     sponsorImages[1].nodeValue = " <img src='images/snocountry_logo.png'> ";
-//     sponsorImages[2].nodeValue = " <img src='images/google-maps-logo.jpg'> ";
-//     sponsorImages[3].nodeValue = " <img src='images/killington-logo.png'> ";
-//     sponsorImages[3].nodeValue = " <img src='images/mammoth-logo.jpg'> ";
-//   }
-// }
-
 var createSponsors = function(src, sponsors) {
   var sponsors = document.getElementById(sponsors);
   var image = document.createElement('img');
   image.setAttribute('src', src);
   sponsors.appendChild(image);
 };
-
 
 // add event listener
 var showSponsors = document.getElementById("show-sponsors");
@@ -52,5 +34,5 @@ showSponsors.addEventListener("click", function(){
   createSponsors('images/google-maps-logo.jpg', 'sponsors');
   createSponsors('images/killington-logo.png', 'sponsors');
   createSponsors('images/mammoth-logo.jpg', 'sponsors');
-}
+  }
 }, false);
