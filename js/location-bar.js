5f35d64350cd4f4f3fcdd6312c8e45ecc9b19ab6
Yogibuff <yogibuff@icloud.com>
2016-01-06 15:18:53 -0800

    #9 Create id for each resort element within location dropdown menu and a corresponding event listener loop for each.

--- js/location-bar.js
+++ js/location-bar.js
@@ -1,124 +1,85 @@
-// Different Options for deploying Event Listeners for each of the 6 States and 24 total Resorts located in the location bar
+/* location bar event listeners */
 
-// adding resorts as child elements of the parent (State abbreviateion) + '-' + 'resorts', ex: "ca-resorts"
+/////////////////   California   ////////////////////
 
-// add event listener
-// var heavenly = document.getElementById("heavenly");
-// heavenly.addEventListener("click", queryHeavenlyCa, false);
+var california = document.getElementById('california');
+  california.addEventListener('focus', addResortListeners);
 
-// Function to set SnoCountry query URL
-//function insertResortUrl() {
-//  var ca = document.getElementByClass("ca-resorts");
-//  ca.firstChild.(selected Resort Heavenly, set variables for state=ca and resort name for api.js to grab and make the request URL) : 
-//    var state = 'ca';
-//    var resort = 'heavenly';
-//  };
-//  ca.secondChild.(select Resort Apline Meadows)
-//    var state = 'ca';
-//    var resort = 'alpineMeadows';
-//  };
-//}
-/*
-Each resort is located at:
-$('locations_box > a') 
+var californiaResortsArray = ["heavenly", "alpineMeadows", "squawValley", "mammoth"];
 
-use "this" to match text from the element to the case where that SnoCountry API is written out fully with the state and resort name inserted
+var addResortListeners = function() {
+  for (var i = 0; i < californiaResortsArray.length; i++){
+    addEventListeners(californiaResortsArray[i] + "SearchButton");
+  }
+};
 
 
-eventListener 'click'
+/////////////////   Colorado   ////////////////////
+
+var colorado = document.getElementById('colorado');
+  colorado.addEventListener('focus', addResortListeners);
+
+var coloradoResortsArray = ["vail", "telluride", "keystone", "breckenridge"];
+
+var addResortListeners = function() {
+  for (var i = 0; i < coloradoResortsArray.length; i++){
+    addEventListeners(coloradoResortsArray[i] + "SearchButton");
+  }
+};
 
 
-// start here
-$(document.body).ready(function () {
-  $(".locations_box > a").on('click', function () {
-    // on 'click', store the text inside the <a> element, which is the resort's name stored as a reference in the variable "resort"
-    var elName = resort;
-    // display the SnoCountry data (yet to be created)
-    $('#snoCountryOutput').css('display', 'visible');
-  });
-});
 
-// List checking the clicked element's name (resort the user wants weather info on)
-if (this = calResort1) {
-  
-}
-if (this = californiaResort2) {
-  
-}
-if (this = californiaResort3) {
-  
-}
-if (this = californiaResort4) {
-  
-}
+/////////////////   Montana   ////////////////////
 
-if (this = coloradoResort1) {
-  
-}
-if (this = coloradoResort2) { 
+var montana = document.getElementById('montana');
+  montana.addEventListener('focus', addResortListeners);
 
-}
-if (this = coloradoResort3) {
-  
-}
-if (this = coloradoResort4) {
-  
-}
+var montanaResortsArray = ["bigSky", "bridgerBowl", "whitefish", "snowbowl"];
 
-// if this element clicked was a montana resort, then find its URL from resort-data.js and send the URL to api.js for querying SnoCountryAPI
-if (this = montanaResort1) {
-  
-}
-if (this = montanaResort2) {
-  
-}
-if (this = montanaResort3) {
-  
-}
-if (this = montanaResort4) {
-  
-}
-// etc...
+var addResortListeners = function() {
+  for (var i = 0; i < montanaResortsArray.length; i++){
+    addEventListeners(montanaResortsArray[i] + "SearchButton");
+  }
+};
 
 
-/* IDEA 2 - possible to add Resort on 'click' listeners to event-listeners.js 
-so that child elements[1-4] send the clicked resort name and state variables for api.js 
-(the SnoCountry API URL) */
+/////////////////   NewMexico   ////////////////////
 
-/*
-// BEGIN on'click' listners for each State's id
-$(document.body).ready(function () {
-  $("#california").on('click', function () {
-    newLocation(39.17852,-120.22769);
-    $('#map').css('height', '400px');
-  });
-  $("#colorado").on('click', function () {
-    newLocation(39.19110,-106.81754);
-  });
-  $("#montana").on('click', function () {
-    newLocation(46.98927,-114.01456);
-  });
-  $("#new-mexico").on('click', function () {
-    newLocation(36.39309,-105.28501);
-  });
-  $("#utah").on('click', function () {
-    newLocation(40.58884,-111.63798);
-  });
-  $("#vermont").on('click', function () {
-    newLocation(43.67757,-72.77982);
-  });
-  // 'click' event on any State checks for jumbotron, if visible - hides it
-  $(".states").on('click', function () {
-    if($('.jumbotron').is(':visible')) {
-      $('.jumbotron').css('display', 'none');
-    }
-  });
-  //sets map to visible when any State clicked
-  $(".states").on('click', function () {
-    if($('#map').not(':visible')) {
-      $('#map').css('opacity', '1');
-    }
-  });
-  
-});
-*/
+var newMexico = document.getElementById('newMexico');
+  newMexico.addEventListener('focus', addResortListeners);
+
+var newMexicoResortsArray = ["taosSkiValley", "apache", "parajito", "angelFire"];
+
+var addResortListeners = function() {
+  for (var i = 0; i < newMexicoResortsArray.length; i++){
+    addEventListeners(newMexicoResortsArray[i] + "SearchButton");
+  }
+};
+
+
+/////////////////   Utah   ////////////////////
+
+var utah = document.getElementById('utah');
+  utah.addEventListener('focus', addResortListeners);
+
+var utahResortsArray = ["alta", "solitude", "parkCity", "snowbird"];
+
+var addResortListeners = function() {
+  for (var i = 0; i < utahResortsArray.length; i++){
+    addEventListeners(utahResortsArray[i] + "SearchButton");
+  }
+};
+
+
+/////////////////   Vermont   ////////////////////
+
+var vermont = document.getElementById('vermont');
+  vermont.addEventListener('focus', addResortListeners);
+
+var vermontResortsArray = ["killington", "jaysPeak", "smugglersNotch", "stratton"];
+
+var addResortListeners = function() {
+  for (var i = 0; i < vermontResortsArray.length; i++){
+    addEventListeners(vermontResortsArray[i] + "SearchButton");
+  }
+};
