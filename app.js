c35f90dd00e74f0715779ca14be298407c1ae244
Yogibuff <yogibuff@icloud.com>
2016-01-11 15:49:56 -0800

    #21 Express route to serve custom 404 page instead of default 404 response.

--- app.js
+++ app.js
@@ -2,18 +2,19 @@
 //  Express Routers  //
 //////////////////////
 
-// require the express module and set host port
+// require the Express module, set host port
 var express = require('express'),
     app = express(),
-    port = 3000;
+    port = 3000,
+// require api.js for implementation of SnoCountry API's weather queries
+    api = require('./js/api.js');
 
-// set a static router to retrieve HTML, CSS, JS, Images
+// app.use('/api', api);
+
+// set static router to retrieve HTML, CSS, JS, Images
 app.use(express.static(__dirname));
 
-// require api.js for implementation of SnoCountry API's weather queries
-// var api = require('./js/api.js');
-// app.use('api.js', api);
-
+// set route to custom 404 Page, overrides default 404 server response
 app.use(function(req, res, next) {
   res.status(404).sendFile(__dirname + '/custom-404.html');
 });
